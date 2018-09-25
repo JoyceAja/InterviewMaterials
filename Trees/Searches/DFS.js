1ffb4735507844f634fc9cab00bf8ebc89c9ac3c
JoyceAja <ajagbe.joyce@gmail.com>
2018-09-25 11:31:13 -0400

    added the iteratives and recursive dfs search approaches

--- Trees/Searches/DFS.js
+++ Trees/Searches/DFS.js
@@ -1,3 +1,37 @@
-const dfs_iterative = () => {
-    
+const dfs_recursive = (root, target) => {
+    //base case
+    if(root.value === target){ 
+        return root
+    }
+    if(root.right){
+        let result = dfs_recursive(root.right, target)
+        if(result){
+            return result
+        }
+    }
+    if(root.left){
+        let result = dfs_recursive(root.right, left)
+        if(result){
+            return result
+        }
+    }
+    return null
+}
+
+
+
+
+const dfs_iterative = (root, target) => {
+    const stack = [root]
+
+    while(stack.length > 0){
+        let node = stack.pop()
+
+        if(node.value === target){
+            return node
+        }
+        if(node.right){stack.push(node.right)}
+        if(node.left){stack.push(node.left)}
+    }
+    return null
 }
